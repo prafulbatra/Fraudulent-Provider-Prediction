467292b435cb59ce6bacde7548fdfcb2e5c0ea51
prafulbatra <68347676+prafulbatra@users.noreply.github.com>
2022-03-05 16:56:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,10 +272,10 @@ uploaded_file2=st.file_uploader(label='Upload Inpatient excel file',key=2)
 uploaded_file3=st.file_uploader(label='Upload Outpatient excel file',key=3)
 uploaded_file4=st.file_uploader(label='Upload Provider excel file',key=4)
 
-with st.form("Fraud Prediction"):
+with st.form("Fraud Prediction",clear_on_submit):
     if ((uploaded_file1 is not None)&(uploaded_file2 is not None)&(uploaded_file3 is not None)&(uploaded_file4 is not None)):
         if(file_check(uploaded_file1,uploaded_file2,uploaded_file3,uploaded_file4)):
-            st.write("Now , you are all set for prediction")
+            st.write("Now , you are all set for prediction. Click Submit Button to get the results")
             uploaded_file1.seek(0)
             beneficiary = pd.read_csv(uploaded_file1)
             uploaded_file2.seek(0)
